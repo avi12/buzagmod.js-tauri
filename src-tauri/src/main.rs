ac1f34f7b59020a2dfe9ee7e10f641a28d52cdad
avi12 <avi6106@gmail.com>
2022-07-10 02:53:43 +0300

    Added `tauri-plugin-fs-extra`

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,11 @@ all(not(debug_assertions), target_os = "windows"),
 windows_subsystem = "windows"
 )]
 
+use tauri_plugin_fs_extra::FsExtra;
+
 fn main() {
     tauri::Builder::default()
+        .plugin(FsExtra::default())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
